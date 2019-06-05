2ec61b065e87e3244a68bb5794f6b6f9eb81094a
JeanLucas <34515830+jeanlucaspl@users.noreply.github.com>
2019-06-05 13:01:16 -0300

    Update alg1-tp.hs

--- alg1-tp.hs
+++ alg1-tp.hs
@@ -1,2 +1,32 @@
-Usuario :: Integer-> Integer->Integer
-Usuario  a b = a+b
+
+type Set a = [a]
+type Usuario = (Integer, String)
+type Relacion = (Usuario, Usuario)
+type Publicacion = (Usuario, String, Set Usuario)
+type RedSocial = (Set Usuario, Set Relacion, Set Publicacion)
+
+usuarios :: RedSocial -> Set Usuario
+usuarios (a,b,c)=a
+
+relaciones :: RedSocial -> Set Relacion
+relaciones (a,b,c)=b
+
+publicaciones :: RedSocial -> Set Publicacion
+publicaciones (a,b,c)=c
+
+idDeUsuario :: Usuario -> Integer
+idDeUsuario (a,b)= a
+
+nombreDeUsuario :: Usuario -> String
+nombreDeUsuario (a,b)=b
+
+usuarioDePublicacion :: Publicacion -> Usuario
+usuarioDePublicacion  (a,b,c)= a
+
+likesDePublicacion :: Publicacion -> Set Usuario  
+likesDePublicacion (a,b,c)=c
+
+nombresDeUsuarios :: RedSocial -> Set String
+nombresDeUsuarios ([a],b,c)= a
+nombresDeUsuarios (a:as,b,c) = a : nombresDeUsuarios(as,b,c)
+
